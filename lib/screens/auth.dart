68889e2c986217d0dfb3100ad9dd219612efa33c
bigmosi <kinyeramo@gmail.com>
2024-02-06 17:17:00 +0300

    setting up

---
+++ lib/screens/auth.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+  @override
+  State<StatefulWidget> createState() {
+   return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: ,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
