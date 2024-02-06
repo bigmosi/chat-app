4c55c38018f60c4f8ece4fe761790901661d28b0
bigmosi <kinyeramo@gmail.com>
2024-02-07 01:17:55 +0300

    created signup page

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -9,6 +9,8 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var _isLogin = true;
+
    @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +21,55 @@ class _AuthScreenState extends State<AuthScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                margin: ,
+                margin: const EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20
+                ),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Email Address'
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Password'
+                          ),
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 12),
+                        ElevatedButton(
+                          onPressed: () {}, 
+                          child: Text( _isLogin ? 'login' : 'Signup'),
+                          ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: Text(_isLogin ? 'Create an account' : 'I already have an account')
+                          )
+                      ],
+                    )),
+                    ),
+                ),
               )
             ],
           ),
