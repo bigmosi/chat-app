1bcdbd7f81ddd17bd12267a2c08d225e91a6da44
bigmosi <kinyeramo@gmail.com>
2024-02-07 14:28:32 +0300

    added auth screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -7,7 +7,7 @@ class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
   @override
   State<StatefulWidget> createState() {
-   return _AuthScreenState();
+    return _AuthScreenState();
   }
 }
 
@@ -20,36 +20,36 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredPassword = '';
 
   void _submit() async {
-    final isValid  = _formKey.currentState!.validate();
+    final isValid = _formKey.currentState!.validate();
 
     if (!isValid) {
       return;
     }
-    
-     _formKey.currentState!.save();
 
-     if (_isLogin) {
-      // log users in
-     } else {
-       try {
-         final userCredentials = await _firebase.createUserWithEmailAndPassword(
-        email: _enteredEmail,
-        password: _enteredPassword
+    _formKey.currentState!.save();
+
+    try {
+      if (_isLogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
         );
         print(userCredentials);
-       } on FirebaseAuthException catch (error) {
-        if (error.code == 'email-already-in-use') {
-          //...
-        }
-         ScaffoldMessenger.of(context).clearSnackBars();
-         ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(error.message ?? 'Authentication failed.')));
-       }
-
-     }
-      
+      }
+    } on FirebaseAuthException catch (error) {
+      if (error.code == 'email-already-in-use') {
+        // Handle email already in use error
+      }
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(error.message ?? 'Authentication failed.')));
+    }
   }
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
@@ -60,11 +60,7 @@ class _AuthScreenState extends State<AuthScreen> {
             children: [
               Container(
                 margin: const EdgeInsets.only(
-                  top: 30,
-                  bottom: 20,
-                  left: 20,
-                  right: 20
-                ),
+                    top: 30, bottom: 20, left: 20, right: 20),
                 width: 200,
                 child: Image.asset('assets/images/chat.png'),
               ),
@@ -74,64 +70,65 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(16),
                     child: Form(
-                      key: _formKey,
-                      child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Email Address'
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                          autocorrect: false,
-                          textCapitalization: TextCapitalization.none,
-                          validator: (value) {
-                             if (value == null || value.trim().isEmpty || !value.contains('@')) {
-                              return 'Please enter a valid email address';
-                             }
+                        key: _formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Email Address'),
+                              keyboardType: TextInputType.emailAddress,
+                              autocorrect: false,
+                              textCapitalization: TextCapitalization.none,
+                              validator: (value) {
+                                if (value == null ||
+                                    value.trim().isEmpty ||
+                                    !value.contains('@')) {
+                                  return 'Please enter a valid email address';
+                                }
 
-                             return null;
-                          },
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _enteredEmail = value!;
+                              },
+                            ),
+                            TextFormField(
+                              decoration:
+                                  const InputDecoration(labelText: 'Password'),
+                              obscureText: true,
+                              validator: (value) {
+                                if (value == null || value.trim().length < 6) {
+                                  return 'Password must be at least 6 character long.';
+                                }
 
-                          onSaved: (value) {
-                              _enteredEmail = value!;
-                          },
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Password'
-                          ),
-                          obscureText: true,
-                          validator: (value) {
-                            if (value == null || value.trim().length < 6) {
-                              return 'Password must be at least 6 character long.';
-                            }
-
-                            return null;
-                          },
-                          onSaved: (value) {
-                              _enteredPassword = value!;
-                          },
-                        ),
-                        const SizedBox(height: 12),
-                        ElevatedButton(
-                          onPressed: _submit,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Theme.of(context).colorScheme.primaryContainer
-                          ),
-                          child: Text( _isLogin ? 'login' : 'Signup'),
-                          ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              _isLogin = !_isLogin;
-                            });
-                          },
-                          child: Text(_isLogin ? 'Create an account' : 'I already have an account')
-                          )
-                      ],
-                    )),
-                    ),
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _enteredPassword = value!;
+                              },
+                            ),
+                            const SizedBox(height: 12),
+                            ElevatedButton(
+                              onPressed: _submit,
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .primaryContainer),
+                              child: Text(_isLogin ? 'login' : 'Signup'),
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _isLogin = !_isLogin;
+                                  });
+                                },
+                                child: Text(_isLogin
+                                    ? 'Create an account'
+                                    : 'I already have an account'))
+                          ],
+                        )),
+                  ),
                 ),
               )
             ],
